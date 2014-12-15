8616ceb827f45e5dcf4aa06c621488bff6de3306
Siva Visakan Sooriyan <sivavisakan@gmail.com>
2014-12-14 16:48:53 -0800

    Initial Commit

---
+++ ImageEditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageEditor
+//
+//  Created by Siva Visakan Sooriyan on 12/14/14.
+//  Copyright (c) 2014 Siva Visakan Sooriyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
